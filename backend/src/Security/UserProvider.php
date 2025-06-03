e104210e299550cb4415104f5ba466fba4f5fd30
Anouar Elammari <93911172+Zoorroo@users.noreply.github.com>
2025-06-03 13:30:22 +0200

    Add backend/src/Security/UserProvider.php 1748950221969

---
+++ backend/src/Security/UserProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\Utilisateur;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class UserProvider implements UserProviderInterface
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function loadUserByUsername(string $username): ?Utilisateur
+    {
+        return $this->entityManager->getRepository(Utilisateur::class)
+            ->findOneBy(['email' => $username]);
+    }
+
+    public function refreshUser(UserInterface $user): UserInterface
+    {
+        if (!$user instanceof Utilisateur) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    public function supportsClass(string $class): bool
+    {
+        return Utilisateur::class === $class || is_subclass_of($class, Utilisateur::class);
+    }
+}
